0086b2964e565f096700e3c43df0890361ef687e
Dananjayan <142473132+Dananjayan-Fortude@users.noreply.github.com>
2023-11-17 10:33:12 +0530

    Fix error status code in user deletion

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -68,7 +68,7 @@ export class UserDetailsComponent {
       (error) => {
         // An error occurred during deletion
         console.error('Error occurred:', error);
-        if (error.status === 403 && error.error.message === 'Forbidden resource') {
+        if (error.status === 401 && error.error.message === 'Forbidden resource') {
           // Handle the "Forbidden resource" error specifically
           console.log('Forbidden resource error');
           this.toast.error('YOU DO NOT HAVE ACCESS TO DELETE', error.error.message, {
