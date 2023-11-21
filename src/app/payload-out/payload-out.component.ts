78db1fd54fa312c785daf24886a736a32f2704b3
Dananjayan <142473132+Dananjayan-Fortude@users.noreply.github.com>
2023-11-21 17:01:36 +0530

    Refactor payload-out component to display additional information

--- src/app/payload-out/payload-out.component.ts
+++ src/app/payload-out/payload-out.component.ts
@@ -11,23 +11,72 @@ export class PayloadOutComponent {
   constructor( private service: PayloadOutService) {
   }
 
-  id : string | undefined;
-  pay: any;
+  id : string = "";
   payloads: any[] = [];
+  error_msg: any[] = [];
+  total: number[] = [];
+  headerID: any;
+  susStep: any;
+  pickStat: any;
+  allocStat: any;
+  pickStatMean: string = "";
 
   getID() {
-    // @ts-ignore
-    this.service.payload(this.id).subscribe(
-      (response: any) => {
-        // console.log(response.length);
-        // this.pay = response[0];
-        // console.log(JSON.parse(this.pay));
-        for (const res in response) {
-          this.payloads.push(JSON.parse(response[res]));
+    this.payloads = [];
+    try {
+      this.service.payload(this.id.toString()).subscribe(
+        (response: any) => {
+          this.susStep = response.suspenededStep;
+          this.pickStat = response.picklistStatus;
+          this.allocStat = response.allocatedStatus;
+          this.headerID = response.headerID;
+          if (this.pickStat === 0){
+            this.pickStatMean = "CREATED"
+          } else if (this.pickStat === 1){
+            this.pickStatMean = "PENDING"
+          } else if (this.pickStat === 2){
+            this.pickStatMean = "NOT_STARTED"
+          } else if (this.pickStat === 3){
+            this.pickStatMean = "READY_TO_ISSUE"
+          } else if (this.pickStat === 4){
+            this.pickStatMean = "ISSUED"
+          } else if (this.pickStat === 5){
+            this.pickStatMean = "SUSPENDED"
+          } else if (this.pickStat === 6){
+            this.pickStatMean = "FAILED"
+          } else if (this.pickStat === 7){
+            this.pickStatMean = "REVERSED"
+          }
+          console.log(response.headerID);
+          if (response && response.payloads && response.payloads.length !== 0) {
+            //console.log(JSON.parse(response.payloads[0]))
+            for (let i = 0; i < response.payloads.length; i++) {
+              for (let x = 0; x < JSON.parse(response.payloads[i]).to.mos.length; x++) {
+                this.total.push(JSON.parse(response.payloads[i]).to.mos[x].quantity)
+              }
+            }
+            for (let i = 0; i < response.payloads.length; i++) {
+              try {
+                const parsedPayload = JSON.parse(response.payloads[i]);
+                this.payloads.push(parsedPayload);
+                this.error_msg.push(response.errors[i]);
+              } catch (parseError) {
+                console.log('Error parsing payload:', parseError);
+              }
+            }
+          } else {
+            console.log('No payload found');
+          }
+        },
+        (error: any) => {
+          console.log('Error in HTTP request:');
+          // Handle the error as needed
         }
-
-      }
-    )
+      );
+    } catch (e) {
+      console.log('Error in getID:');
+      // Handle the error as needed
+    }
   }
 
   idInput(eventData: any) {
