72f6e29f012f8f902e3f90f3a4a27377143806ff
Dananjayan <142473132+Dananjayan-Fortude@users.noreply.github.com>
2023-11-21 11:52:56 +0530

    Add payload-out component and service

---
+++ src/app/payload-out/payload-out.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payload-out',
+  templateUrl: './payload-out.component.html',
+  styleUrls: ['./payload-out.component.css']
+})
+export class PayloadOutComponent {
+
+}
